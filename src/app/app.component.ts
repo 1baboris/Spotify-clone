14c158adff183085578f986150e29925b7044ae5
baboris <boris.benachour@gmail.com>
2024-04-27 11:30:14 +0200

    création du frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+import { LibraryComponent } from './layout/library/library.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FontAwesomeModule,
+    NavigationComponent,
+    LibraryComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spotify-clone';
+
+  private faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome(): void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
